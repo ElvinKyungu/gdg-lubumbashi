d2152a5aafef2c376dce942f01548f5f0f8942d4
Elvin Kyungu <elvinkyungu.75@gmail.com>
2024-11-15 18:01:27 +0200

    add responsive navbar

---
+++ components/base/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="bg-slate-100  py-10 px-5 md:px-20">
+    <div class="grid grid-cols-12 md:flex-row justify-between gap-10">
+      <div class="col-span-4 space-y-4">
+        <nuxt-link to="/" class="flex items-center space-x-2">
+          <img src="@/assets/images/logo.png" alt="Logo" class="w-20">
+          <span class="text-lg font-medium">GDG Lubumbashi</span>
+        </nuxt-link>
+        <p class="text-sm text-gray-700">
+          GDG Lubumbashi is a local tech community focused on learning, sharing, and innovating with Google technologies and beyond.
+        </p>
+      </div>
+
+      <div class="col-span-4 flex  justify-center">
+        <ul class="flex space-x-6">
+          <li>
+            <nuxt-link to="/" class="hover:text-green-400 transition">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about" class="hover:text-green-400 transition">About</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/services" class="hover:text-green-400 transition">Team</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/contact" class="hover:text-green-400 transition">Contact</nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div class="col-span-4 flex  justify-center">
+        <div class="flex space-x-4">
+          <nuxt-link to="https://twitter.com" target="_blank" class="hover:text-green-400">
+            <IconsX class="text-2xl" />
+          </nuxt-link>
+          <nuxt-link to="https://linkedin.com" target="_blank" class="hover:text-green-400">
+            <IconsLinkedin class="text-2xl" />
+          </nuxt-link>
+          <nuxt-link to="https://youtube.com" target="_blank" class="hover:text-green-400">
+            <IconsYoutube class="text-2xl" />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+    <div class="mt-10 border-t border-gray-600 pt-4 text-center text-sm text-gray-700">
+      &copy; 2024 GDG Lubumbashi. All rights reserved.
+    </div>
+  </footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
 <template>
   <BaseHeader/>
+  <main class="mt-20 mb-10">
+    <h1 class="text-7xl font-bold text-center my-10">DevFest Lubumbashi 2024</h1>
+    <div class="grid space-x-20 grid-cols-12">
+      <div class="col-span-2">
+      </div>
+      <div class="col-span-4">
+        <img class="shad object-contain shadow-2xl rounded-lg" src="@/assets/images/eventHeader.gif" alt="">
+      </div>
+      <div class="col-span-4">
+        <p class="col-span-9 rounded-lg">
+          DevFests are local tech conferences hosted by Google Developer Groups (GDG) around the world. Each DevFest event is crafted by its local organizers to fit the needs and interests of its local developer community. Whether it be through hands-on learning experiences, technical talks delivered in local languages by experts, or by simply meeting fellow local developers, DevFest attendees learn how to build together and innovate on Google’s developer tools.
+        </p>
+        <ul class="flex space-x-4 mt-5">
+          <li class="flex gap-2">
+            <IconsCalendar/>
+            <span>
+              November 9th, 2024
+            </span>
+          </li>
+          <li class="flex gap-2">
+            <IconsMap/>
+            <span class="border-b">
+              Concordia University Conference Center
+            </span>
+          </li>
+        </ul>
+        <button class="py-3 px-5 mt-5 bg-black text-white rounded-full">Get Tickets</button>
+      </div>
+      <div class="col-span-2">
+      </div>
+    </div>
+  </main>
+  <BaseFooter/>
 </template>
